9de4dae566ba2e87f1030f141ad041951e6c4012
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-07-02 22:22:59 +0100

    fix: add validation to Timesheet model

--- TimesheetSystem/Models/TimesheetEntry.cs
+++ TimesheetSystem/Models/TimesheetEntry.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TimesheetSystem.Models;
 
 public class TimesheetEntry
 {
     public Guid Id { get; set; }
+    
+    [Required]
     public Guid UserId { get; set; }
+    
+    [Required]
     public Guid ProjectId { get; set; }
+    
+    [Required]
     public DateOnly Date { get; set; }
+    
+    [Required]
+    [Range(0.1, 24.0, ErrorMessage = "Hours Worked must be greater than zero and less than 24 hours")]
     public Decimal HoursWorked { get; set; }
+    
     public string? Description { get; set; }
 }
