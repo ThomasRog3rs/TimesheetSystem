c212e3fb51c2fcdfa54ffc602eaff143fb0dc04e
ThomasRog3rs <Mr.Thomas.Rogers@proton.me>
2025-07-03 22:10:25 +0100

    fix: make sure date is not in the future

--- TimesheetSystem/Models/TimesheetEntry.cs
+++ TimesheetSystem/Models/TimesheetEntry.cs
@@ -13,6 +13,7 @@ public class TimesheetEntry
     public Guid? ProjectId { get; set; }
     
     [Required]
+    [NotInFuture]
     public DateOnly? Date { get; set; }
     
     [Required]
@@ -20,4 +21,26 @@ public class TimesheetEntry
     public Decimal HoursWorked { get; set; }
     
     public string? Description { get; set; }
+}
+
+//custom validation for not in the future (would be better to move this to a validation directory or something similar)
+public class NotInFutureAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is null)
+            return true;
+
+        if (value is DateOnly date)
+        {
+            return date <= DateOnly.FromDateTime(DateTime.Today);
+        }
+
+        return false;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return $"{name} cannot be in the future.";
+    }
 }
